d1a99bf5551bcb292298f8154efc17b0ccea6474
Michael <michael.helman@gmail.com>
2014-01-14 07:51:10 -0800

    layout changes

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -8,12 +8,6 @@ describe PagesController do
       response.should be_success
     end
     
-    it "should have the right title" do
-            get 'home'
-            response.should have_selector("title",
-                                          :content => "#{@base_title} | Home")
-    end
-    
   end
 
   describe "GET 'contact'" do
