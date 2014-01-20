7acd756b3261e5870b57761cb2c1efd3d16c55e1
Michael <michael.helman@gmail.com>
2014-01-20 09:11:26 -0800

    added password

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140116161920) do
+ActiveRecord::Schema.define(:version => 20140120150859) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+    t.string   "encrypted_password"
   end
 
 end
