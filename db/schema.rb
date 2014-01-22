2b59444fc1f41544f3d8f6b395d7b7bf535c4f80
Michael <michael.helman@gmail.com>
2014-01-22 07:30:58 -0800

    user password

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140120150859) do
+ActiveRecord::Schema.define(:version => 20140122012542) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20140120150859) do
     t.datetime "created_at",         :null => false
     t.datetime "updated_at",         :null => false
     t.string   "encrypted_password"
+    t.string   "salt"
   end
 
 end
