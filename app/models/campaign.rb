78370d611e48a63f93bf2183eabd003b1a4d17d3
Michael <michael.helman@gmail.com>
2014-02-08 13:02:49 -0800

    campaign validations

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,5 +2,6 @@ class Campaign < ActiveRecord::Base
   attr_accessible :name, :targeting_string
   belongs_to :user
   validates :user_id, presence: true
+  validates :name, presence: true
   default_scope -> { order('created_at DESC') }
 end
